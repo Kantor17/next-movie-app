14006aa4343f9a2fa55a4095b960b24d744b0514
Maksym_Kantor <max.kantor72@gmail.com>
2023-03-10 00:10:03 +0200

    init: create basic project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@mui/material";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <Container>Hello</Container>
+};
+
+export default Home;
